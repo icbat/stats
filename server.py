adb09b486142e9e19e46acaed97f3b2c2f5954b3
Evan Cobb <cobb.evan@gmail.com>
2016-07-18 15:14:08 -0500

    adding CORS to allow ajax from the apps

--- server.py
+++ server.py
@@ -6,6 +6,24 @@ from os import environ
 import time
 
 print ("Initializing")
+
+class EnableCors(object):
+    name = 'enable_cors'
+    api = 2
+
+    def apply(self, fn, context):
+        def _enable_cors(*args, **kwargs):
+            # set CORS headers
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Methods'] = 'POST'
+            response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+            if bottle.request.method != 'OPTIONS':
+                # actual request; reply with the actual response
+                return fn(*args, **kwargs)
+
+        return _enable_cors
+
 app = Bottle()
 
 @app.post("/<collectionName>")
@@ -34,6 +52,8 @@ if __name__ == "__main__":
     print ("Connecting to mongo")
     plugin = MongoPlugin(uri=mongo_uri, db="mydb", json_mongo=True)
     app.install(plugin)
+    print ("Enabling CORS for AJAX requests")
+    app.install(EnableCors())
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", type=int, default="5000")
