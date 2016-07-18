c60dd93440293a0fd18c9c5049d0a47194381304
Evan Cobb <cobb.evan@gmail.com>
2016-07-18 16:18:41 -0500

    trying allowing everything through CORS

--- server.py
+++ server.py
@@ -16,8 +16,8 @@ class EnableCors(object):
         def _enable_cors(*args, **kwargs):
             # set CORS headers
             response.headers['Access-Control-Allow-Origin'] = '*'
-            response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
-            response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+            response.headers['Access-Control-Allow-Methods'] = '*'
+            response.headers['Access-Control-Allow-Headers'] = '*'
 
             if bottle.request.method != 'OPTIONS':
                 # actual request; reply with the actual response
