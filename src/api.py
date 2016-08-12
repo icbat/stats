8f2f7bca48fcb7c3a339dd5cced13e04a4a1fd13
Evan Cobb <cobb.evan@gmail.com>
2016-08-12 00:27:07 -0500

    refactoring api/logic layer even more!

--- src/api.py
+++ src/api.py
@@ -6,13 +6,13 @@ class stats:
         self.removedField = "_id"
 
     def cleanse(self, data):
+        """Given a list of documents with uuid at the top level, returns a list of documents without uuids defined in ignoredUUIDs and without internal IDs"""
+        return [self.__removeIDs(doc) for doc in data if doc['uuid'] not in self.ignoredUUIDs]
+
+    def __removeIDs(self, document):
         """Removes the mongodb internal id from items returned to the user"""
-        """Given a list of documents with uuid at the top level, returns a list of documents without uuids defined in ignoredUUIDs"""
-        output = []
-        for document in data:
-            if document['uuid'] not in self.ignoredUUIDs:
-                duplicate = copy.deepcopy(document)
-                if self.removedField in duplicate:
-                    del duplicate[self.removedField]
-                output.append(duplicate)
-        return output
+        if self.removedField in document:
+            duplicate = copy.deepcopy(document)
+            del duplicate[self.removedField]
+            return duplicate
+        return document
